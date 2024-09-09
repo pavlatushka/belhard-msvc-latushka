1459d9f49b8d95cc4877c7c04db9caa657f048ea
Latushkopa <latushkopa@ddc.by>
2024-09-09 09:46:45 +0300

    add Service Discovery and register other services inside it

---
+++ service-discovery/src/test/java/by/latushka/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.latushka.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
